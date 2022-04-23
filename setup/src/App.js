ce0206bb11a2f5a477a84adccf1c70c33d3c686e
Dino Hadzic <zgdino@yahoo.com>
2022-04-23 04:57:47 -1000

    initial setup

--- setup/src/App.js
+++ setup/src/App.js
@@ -2,7 +2,19 @@ import React, { useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 
 function App() {
-  return <h2>markdown preview starter</h2>
+  const [markdown, setMarkdown] = useState('## markdown preview')
+  return (
+    <main>
+      <section className='markdown'>
+        <textarea
+          className='input'
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        ></textarea>
+        <article className="result">{markdown}</article>
+      </section>
+    </main>
+  )
 }
 
 export default App
